d9e9bc31c512d005060dc6bfcb33e3894b238e50
wntjr0219 <90208937+wntjr0219@users.noreply.github.com>
2021-09-14 18:20:23 +0900

    Drill05완료
    https://youtu.be/G5hv9QoIf9s
    2018182017송주석

---
+++ Drill05/Drill05.py
@@ -0,0 +1,34 @@
+import turtle
+
+def drunken_up_move():
+    turtle.setheading(90)
+    turtle.forward(50)
+    turtle.stamp()
+
+def drunken_right_move():
+    turtle.setheading(0)
+    turtle.forward(50)
+    turtle.stamp()
+
+def drunken_left_move():
+    turtle.setheading(180)
+    turtle.forward(50)
+    turtle.stamp()
+
+def drunken_down_move():
+    turtle.setheading(270)
+    turtle.forward(50)
+    turtle.stamp()
+
+
+
+def restart():
+    turtle.reset()
+
+turtle.shape("turtle")
+turtle.onkeypress(drunken_up_move,"Up")
+turtle.onkeypress(drunken_right_move,"Right")
+turtle.onkeypress(drunken_left_move,"Left")
+turtle.onkeypress(drunken_down_move,"Down")
+turtle.onkeypress(restart,"Escape")
+turtle.listen()
