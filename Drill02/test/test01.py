f4d046f0362bebb910b599a1455df4dcf0a72a23
wntjr0219 <90208937+wntjr0219@users.noreply.github.com>
2021-10-07 13:27:35 +0900

    짜투리

---
+++ Drill02/test/test01.py
@@ -0,0 +1,14 @@
+alist=['hello', 'world']
+astr='my name is'
+print(alist)
+print(alist[::-1])
+print(astr)
+print(astr[-2:-8:-1])
+adic={'score':80, 'number': 14}
+print(adic)
+print('your score:',adic['score'])
+atup=(1,2,3)
+print(atup,'  *2:', atup*2)
+aset1={1,2,3}
+aset2={3,4,5}
+print(aset1,aset2,'  a1|a2:',aset1|aset2)
---
+++ Drill06/Lecture04_2D_Rendering/character_moves_3.py
@@ -0,0 +1,25 @@
+from pico2d import *
+import math
+
+open_canvas()
+
+grass=load_image('grass.png')
+character=load_image('character.png')
+
+x=0
+y=90
+
+#def go1(x,y):
+#   clear_canvas_now()
+ #   grass.draw_now(400,30)
+  #  character.draw_now(x,y)
+   # delay(0.01)
+    
+
+    
+#while(1):
+#    go1(x,y)
+#    x=x+2
+
+
+close_canvas()
